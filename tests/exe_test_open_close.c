0409b33bf89f3b3243416bd70cb2c0b9db395317
Joachim Metz <joachim.metz@gmail.com>
2016-04-23 07:03:42 +0200

    Worked on tests.

--- tests/exe_test_open_close.c
+++ tests/exe_test_open_close.c
@@ -42,7 +42,42 @@ int exe_test_single_open_close_file(
 	libcerror_error_t *error = NULL;
 	libexe_file_t *file      = NULL;
 	static char *function    = "exe_test_single_open_close_file";
+	char *access_string      = NULL;
 	int result               = 0;
+
+	if( access_flags == LIBEXE_OPEN_READ )
+	{
+		access_string = "read";
+	}
+	else if( access_flags == LIBEXE_OPEN_WRITE )
+	{
+		access_string = "write";
+	}
+	else
+	{
+		access_string = "UNKNOWN";
+	}
+	fprintf(
+	 stdout,
+	 "Testing single open close of: " );
+
+	if( filename != NULL )
+	{
+		fprintf(
+		 stdout,
+		 "%" PRIs_LIBCSTRING_SYSTEM "",
+		 filename );
+	}
+	else
+	{
+		fprintf(
+		 stdout,
+		 "NULL" );
+	}
+	fprintf(
+	 stdout,
+	 " with access: %s\t",
+	 access_string );
 
 	if( libexe_file_initialize(
 	     &file,
@@ -156,7 +191,42 @@ int exe_test_multi_open_close_file(
 	libcerror_error_t *error = NULL;
 	libexe_file_t *file      = NULL;
 	static char *function    = "exe_test_multi_open_close_file";
+	char *access_string      = NULL;
 	int result               = 0;
+
+	if( access_flags == LIBEXE_OPEN_READ )
+	{
+		access_string = "read";
+	}
+	else if( access_flags == LIBEXE_OPEN_WRITE )
+	{
+		access_string = "write";
+	}
+	else
+	{
+		access_string = "UNKNOWN";
+	}
+	fprintf(
+	 stdout,
+	 "Testing multi open close of: " );
+
+	if( filename != NULL )
+	{
+		fprintf(
+		 stdout,
+		 "%" PRIs_LIBCSTRING_SYSTEM "",
+		 filename );
+	}
+	else
+	{
+		fprintf(
+		 stdout,
+		 "NULL" );
+	}
+	fprintf(
+	 stdout,
+	 " with access: %s\t",
+	 access_string );
 
 	if( libexe_file_initialize(
 	     &file,
@@ -336,11 +406,6 @@ int main( int argc, char * const argv[] )
 
 	/* Case 0: single open and close of a file using filename
 	 */
-	fprintf(
-	 stdout,
-	 "Testing single open close of: %s with access: read\t",
-	 source );
-
 	if( exe_test_single_open_close_file(
 	     source,
 	     LIBEXE_OPEN_READ,
@@ -352,10 +417,6 @@ int main( int argc, char * const argv[] )
 
 		return( EXIT_FAILURE );
 	}
-	fprintf(
-	 stdout,
-	 "Testing single open close of: NULL with access: read\t" );
-
 	if( exe_test_single_open_close_file(
 	     NULL,
 	     LIBEXE_OPEN_READ,
@@ -367,11 +428,6 @@ int main( int argc, char * const argv[] )
 
 		return( EXIT_FAILURE );
 	}
-	fprintf(
-	 stdout,
-	 "Testing single open close of: %s with access: write\t",
-	 source );
-
 	if( exe_test_single_open_close_file(
 	     source,
 	     LIBEXE_OPEN_WRITE,
@@ -385,11 +441,6 @@ int main( int argc, char * const argv[] )
 	}
 	/* Case 1: multiple open and close of a file using filename
 	 */
-	fprintf(
-	 stdout,
-	 "Testing multi open close of: %s with access: read\t",
-	 source );
-
 	if( exe_test_multi_open_close_file(
 	     source,
 	     LIBEXE_OPEN_READ,
