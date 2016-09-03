3603349cd18daea78f5871b5b28e9159ed695bd4
Joachim Metz <joachim.metz@gmail.com>
2016-09-03 20:11:52 +0200

    Worked on tests.

--- tests/exe_test_get_version.c
+++ tests/exe_test_get_version.c
@@ -20,7 +20,6 @@
  */
 
 #include <common.h>
-#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
@@ -28,6 +27,7 @@
 
 #include "exe_test_libcstring.h"
 #include "exe_test_libexe.h"
+#include "exe_test_macros.h"
 #include "exe_test_unused.h"
 
 /* Tests retrieving the library version
@@ -46,11 +46,15 @@ int exe_test_get_version(
 	          LIBEXE_VERSION_STRING,
 	          9 );
 
-	if( result != 0 )
-	{
-		return( 0 );
-	}
+	EXE_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
 	return( 1 );
+
+on_error:
+	return( 0 );
 }
 
 /* The main program
@@ -68,10 +72,13 @@ int main(
 	EXE_TEST_UNREFERENCED_PARAMETER( argc )
 	EXE_TEST_UNREFERENCED_PARAMETER( argv )
 
-	if( exe_test_get_version() != 1 )
-	{
-		return( EXIT_FAILURE );
-	}
+	EXE_TEST_RUN(
+	 "libexe_get_version",
+	 exe_test_get_version() )
+
 	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
 }
 
--- tests/exe_test_memory.h
+++ tests/exe_test_memory.h
@@ -1,5 +1,5 @@
 /*
- * Notification functions
+ * Memory allocation functions for testing
  *
  * Copyright (C) 2011-2016, Joachim Metz <joachim.metz@gmail.com>
  *
@@ -19,45 +19,32 @@
  * along with this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if !defined( _LIBEXE_NOTIFY_H )
-#define _LIBEXE_NOTIFY_H
+#if !defined( _EXE_TEST_MEMORY_H )
+#define _EXE_TEST_MEMORY_H
 
 #include <common.h>
-#include <file_stream.h>
-#include <types.h>
-
-#include "libexe_extern.h"
-#include "libexe_libcerror.h"
 
 #if defined( __cplusplus )
 extern "C" {
 #endif
 
-#if !defined( HAVE_LOCAL_LIBEXE )
+#if defined( HAVE_GNU_DL_DLSYM ) && defined( __GNUC__ ) && !defined( __clang__ )
 
-LIBEXE_EXTERN \
-void libexe_notify_set_verbose(
-      int verbose );
+#define HAVE_EXE_TEST_MEMORY		1
 
-LIBEXE_EXTERN \
-int libexe_notify_set_stream(
-     FILE *stream,
-     libcerror_error_t **error );
+extern int exe_test_malloc_attempts_before_fail;
 
-LIBEXE_EXTERN \
-int libexe_notify_stream_open(
-     const char *filename,
-     libcerror_error_t **error );
+extern int exe_test_memcpy_attempts_before_fail;
 
-LIBEXE_EXTERN \
-int libexe_notify_stream_close(
-     libcerror_error_t **error );
+extern int exe_test_memset_attempts_before_fail;
 
-#endif /* !defined( HAVE_LOCAL_LIBEXE ) */
+extern int exe_test_realloc_attempts_before_fail;
+
+#endif /* defined( HAVE_GNU_DL_DLSYM ) && defined( __GNUC__ ) && !defined( __clang__ ) */
 
 #if defined( __cplusplus )
 }
 #endif
 
-#endif /* !defined( _LIBEXE_NOTIFY_H ) */
+#endif /* !defined( _EXE_TEST_MEMORY_H ) */
 
