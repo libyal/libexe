0bf56582c89ad2a0ff12ee09f0e9289f15a4f34a
Joachim Metz <joachim.metz@gmail.com>
2017-03-06 08:06:53 +0100

    Added test data synchronization script

---
+++ synctestdata.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Script that synchronizes the local test data
+#
+# Version: 20161009
+
+TEST_SET="public";
+TEST_INPUT_DIRECTORY="tests/input";
+TEST_FILES="nowrc_test.dll wrc_test.dll";
+
+mkdir -p "${TEST_INPUT_DIRECTORY}/${TEST_SET}";
+
+for TEST_FILE in ${TEST_FILES};
+do
+	URL="https://github.com/libyal/testdata/blob/master/pe_coff/${TEST_FILE}?raw=true";
+
+	curl -L -o "${TEST_INPUT_DIRECTORY}/${TEST_SET}/${TEST_FILE}" ${URL};
+done
+
