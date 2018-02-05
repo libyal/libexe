10eb860d3169e4056ec768519dbb6e48a97b7bb8
Joachim Metz <joachim.metz@gmail.com>
2018-02-05 06:49:35 +0100

    Applied updates

--- libexe/exe_section_table.h
+++ libexe/exe_section_table.h
@@ -1,7 +1,7 @@
 /*
  * The section table definition of an executable (EXE) file
  *
- * Copyright (C) 2011-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
