a1d67a31c8639dc00d9e34f0aaad236dfe9633d6
Joachim Metz <joachim.metz@gmail.com>
2021-03-08 06:19:26 +0100

    Worked on tests

--- synctestdata.sh
+++ synctestdata.sh
@@ -11,7 +11,7 @@ mkdir -p "${TEST_INPUT_DIRECTORY}/${TEST_SET}";
 
 for TEST_FILE in ${TEST_FILES};
 do
-	URL="https://github.com/libyal/testdata/blob/master/pe_coff/${TEST_FILE}?raw=true";
+	URL="https://github.com/libyal/testdata/blob/main/pe_coff/${TEST_FILE}?raw=true";
 
 	curl -L -o "${TEST_INPUT_DIRECTORY}/${TEST_SET}/${TEST_FILE}" ${URL};
 done
