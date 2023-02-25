f774dc5c913fc814cd46cf7b04e181c5132ef9a9
Joachim Metz <joachim.metz@gmail.com>
2023-02-25 04:52:50 +0100

    Worked on tests

--- tests/exe_test_section.c
+++ tests/exe_test_section.c
@@ -27,15 +27,6 @@
 #include <stdlib.h>
 #endif
 
-#if defined( TIME_WITH_SYS_TIME )
-#include <sys/time.h>
-#include <time.h>
-#elif defined( HAVE_SYS_TIME_H )
-#include <sys/time.h>
-#else
-#include <time.h>
-#endif
-
 #include "exe_test_libcerror.h"
 #include "exe_test_libexe.h"
 #include "exe_test_macros.h"
