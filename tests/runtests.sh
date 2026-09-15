fc370b241c14a64f65c65ed7c2117c1a7e1c3124
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 08:58:16 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libexe/.libs/libexe.1.dylib && test -f ./pyexe/.libs/pyexe.so
+if [ -f "${PWD}/libexe/.libs/libexe.1.dylib" ] && [ -f ./pyexe/.libs/pyexe.so ]
 then
-	install_name_tool -change /usr/local/lib/libexe.1.dylib ${PWD}/libexe/.libs/libexe.1.dylib ./pyexe/.libs/pyexe.so
+    install_name_tool -change /usr/local/lib/libexe.1.dylib "${PWD}/libexe/.libs/libexe.1.dylib" ./pyexe/.libs/pyexe.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
